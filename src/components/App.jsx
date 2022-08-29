2189b4ef36826893e4c66d0314477c52a0ba4aad
RoRomario360 <fotoromario@gmail.com>
2022-08-29 23:10:00 +0300

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+import ExercisesDetail from 'pages/ExercisesDetail';
+import Home from 'pages/Home';
+import Navbar from './Navbar/Navbar';
+import Footer from './Footer/Footer';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExercisesDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
   );
 };
---
+++ src/components/SearchExercises/SearchExercises.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import { Box, Button, Stack, TextField, Typography } from '@mui/material';
+
+const SearchExercises = () => {
+  return (
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px ">
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: '44px', xs: '30px' } }}
+        mb="50px"
+        textAlign="center"
+      >
+        The best Exercises
+        <br /> for You
+      </Typography>
+      <Box position="relative" mb="72px">
+        <TextField
+          sx={{
+            input: { fontWeight: '700', border: 'none', borderRadius: '4px' },
+            width: { lg: '800px', xs: '350px' },
+            backgroundColor: '#fffff',
+            borderRadius: '40px',
+          }}
+          height="76px"
+          value=""
+          onChange={e => {}}
+          placeholder="Search exercises"
+          type="text"
+        />
+        <Button
+          className="search__btn"
+          sx={{
+            bgcolor: '#FF2625',
+            color: 'fff',
+            textTransform: 'none',
+            width: { lg: '175px', xs: '80px' },
+            fontSize: { lg: '20px', xs: '14px' },
+            height: '56px',
+            position: 'absolute',
+          }}
+        >
+          Search
+        </Button>
+      </Box>
+    </Stack>
+  );
+};
+
+export default SearchExercises;
