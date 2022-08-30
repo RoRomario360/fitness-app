2d95f12588718fc64491b1870b84445335ae416f
RoRomario360 <fotoromario@gmail.com>
2022-08-30 19:42:17 +0300

    commit

--- src/components/SearchExercises/SearchExercises.jsx
+++ src/components/SearchExercises/SearchExercises.jsx
@@ -1,13 +1,37 @@
 import React, { useEffect, useState } from 'react';
 import { Box, Button, Stack, TextField, Typography } from '@mui/material';
-import { SearchOffSharp } from '@mui/icons-material';
+import { exerciseOptions, fetchData } from 'utils/fetchData';
 
 const SearchExercises = () => {
   const [search, setSearch] = useState('');
+  const [exercises, setExercises] = useState([]);
+  const [bodyParts, setBodyParts] = useState([]);
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const bodyPartsData = await fetchData(
+        'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
+        exerciseOptions
+      );
+      setBodyParts(['all', ...bodyPartsData]);
+    };
+  }, []);
 
   const handleSearch = async () => {
     if (search) {
-      // const exerciseData = await fetchData();
+      const exerciseData = await fetchData(
+        'https://exercisedb.p.rapidapi.com/exercises',
+        exerciseOptions
+      );
+      const searchExercises = exerciseData.filter(
+        exercise =>
+          exercise.name.toLowerCase().includes(search) ||
+          exercise.target.toLowerCase().includes(search) ||
+          exercise.equipment.toLowerCase().includes(search) ||
+          exercise.bodyPart.toLowerCase().includes(search)
+      );
+      setSearch('');
+      setExercises(searchExercises);
     }
   };
 
